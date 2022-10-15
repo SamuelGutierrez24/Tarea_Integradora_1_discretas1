642cd2f406499b391abd7b676388cd9c3358bcd7
Collin Gonzalez <Collin@PC-de-alguien.local>
2022-10-15 17:32:31 -0500

    MainTest

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -20,33 +20,74 @@ public class Main {
             
             int option;
             
-            do {
-            	
-            	option = ppal.showMenu();
-                ppal.selecOption(option);
-                
-            } while(option != 0);
-
+            ppal.showMenu();
+        
            
         }
 
-       public int showMenu() {
-    	   
-    	   int option = 0;
+       public void showMenu() {
     	   
     	   System.out.println("Welcome!\n" );
-        
-    	   System.out.println("Select what do you ant to do:\n\n" + "(1) Enter to a lab\n" + "(2) Search client\n" + "(0) Exit" );
     	   
-    	   option = sc.nextInt();
+    	   boolean stopFlag = false;
+
+           while (!stopFlag) {
+        	   
+        	   System.out.println("Select what do you want to do:\n\n" + "(1) Enter to a lab\n" + "(2) Search client\n" + "(0) Exit" );
+
+               int mainOption = sc.nextInt();
+
+               switch (mainOption) {
+                   case 1:
+                	   
+                	   System.out.println("Select wich lab you want to go:\n\n" + "(1) Hematology\n" + "(2) General purpose\n" + "(0) Back");
+                	   
+                	   int lab = 0;  
+                	   
+                	   boolean stopFlag2 = false;
+                	   
+                	   while (!stopFlag2) {
+                	   
+                		   switch(lab) {
+                	   
+                		   case 1:
+                		   
+                			   break;
+                		   case 2:
+                		   
+                			   break;
+                		   default:
+                		            		   
+                			   break;
+                		   case 0:
+                			   
+                			   stopFlag2 = true;
+                			   break;		   
+                		   }
+                		   
+                	   }
+                	   
+                   
+                       break;
+                   case 2:
+                   	
+                       
+                       
+                       break;
+                   case 0:
+                   	
+                       System.out.println("Bye.");
+                       
+                       stopFlag = true;
+                       break;
+               }
+           }
     	   
-    	   sc.nextLine();
-    	   
-    	   return option;
+  
         
        } 
 
-       public void selecOption(int option){
+       public void selectOption(int option){
     	   
     	   switch(option) {
     	   
